5fed74a4b706932d42c7f50ca0b750cbaface42d
Rnveer007 <rvs523474@gmail.com>
2025-03-27 13:38:09 +0530

    updated

--- throttlingDemo.js
+++ throttlingDemo.js
@@ -3,14 +3,44 @@ const COUNT = 15;
 const URL =
     "https://api.unsplash.com/photos/random/?client_id " = KEY + "&count" + COUNT;
 
+const loaded = false;
+
 const postsDiv = document.querySelector("#posts");
+const loader = document.querySelector("#loader");
+
 
 async function getPhotos() {
+    loader.style.display = "block";
+    loaded = false
     const response = await fetch(URL);
     const result = await response.json()
     displayPhotos(result)
 }
 
-function displayPhotos(data) {
+function displayPhotos(arr) {
+    const fragment = document.createDocumentFragment();
+    arr.forEach((object) => {
+        const anchor = document.createElement("a")
+        anchor.href = object.links.self;
+
+        const img = document.createElement(img);
+        img.src = object.urls.regular;
+
+        anchor.append(img);
+        fragment.append(anchor);
+    });
+    postsDiv.append(fragment);
+    loader.style.display = "none";
+    loaded = true;
 
 }
+
+window.addEventListener("scroll", () => {
+    if (
+        window.scrollY + window.innerHeight >= document.body.offsetHeight - 100 && loaded
+    ) {
+        getPhotos()
+    }
+})
+
+getPhotos();
